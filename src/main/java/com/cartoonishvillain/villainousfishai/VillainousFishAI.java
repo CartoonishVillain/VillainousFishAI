56e5f47f0491a6fccb64e8d71ea25ea6e80c109d
Cartoonish Villain <jedijoe93@gmail.com>
2021-08-26 13:48:08 -0400

    Bug fixing

--- src/main/java/com/cartoonishvillain/villainousfishai/VillainousFishAI.java
+++ src/main/java/com/cartoonishvillain/villainousfishai/VillainousFishAI.java
@@ -1,9 +1,6 @@
 package com.cartoonishvillain.villainousfishai;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -57,12 +54,5 @@ public class VillainousFishAI
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
+
 }
